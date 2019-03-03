c1c9228b0be64e67692dfb18a80d50b6e3b25b44
Oxana Bykova <bykova.oksana@gmail.com>
2019-03-03 14:37:02 -0500

    tanks

--- tanks.tsx
+++ tanks.tsx
@@ -2,16 +2,31 @@ import * as React from 'react';
 import { FieldConcern, FieldSeed } from './data/field';
 import { TextField } from './text-field';
 
-export type TanksConcern = TanksToSaveConcern | {
-    about: 'air12L', air12L: FieldConcern;
-};
+export type TanksConcern =
+    | TanksToSaveConcern
+    | WhenAir12LConsern
+    | WhenAir15LConsern
+    | WhenNitrox12LConsern
+    | WhenNitrox15LConsern;
 
 export interface TanksToSaveConcern {
     about: 'tanks-to-save';
 }
-export interface TanksValueConcern {
-    about: 'tanks-value-concern';
-    what: FieldSeed;
+export interface WhenAir12LConsern {
+    about: 'air12L';
+    air12L: FieldConcern;
+}
+export interface WhenAir15LConsern {
+    about: 'air15L';
+    air15L: FieldConcern;
+}
+export interface WhenNitrox12LConsern {
+    about: 'nitrox12L';
+    nitrox12L: FieldConcern;
+}
+export interface WhenNitrox15LConsern {
+    about: 'nitrox15L';
+    nitrox15L: FieldConcern;
 }
 export interface TanksSeed {
     kind: 'tanks';
@@ -27,37 +42,35 @@ export interface TanksProps {
 }
 
 export class Tanks extends React.Component<TanksProps> {
-
-
     private whenAir12L = (concern: FieldConcern) => {
         this.props.when({ about: 'air12L', air12L: concern });
     }
+    private whenAir15L = (concern: FieldConcern) => {
+        this.props.when({ about: 'air15L', air15L: concern });
+    }
+    private whenNitrox12L = (concern: FieldConcern) => {
+        this.props.when({ about: 'nitrox12L', nitrox12L: concern });
+    }
+    private whenNitrox15L = (concern: FieldConcern) => {
+        this.props.when({ about: 'nitrox15L', nitrox15L: concern });
+    }
 
     render() {
-        const { air12L } = this.props.seed;
-        const air12Lvalue = air12L.value || air12L.text;
-        if (air12Lvalue === air12L.text) {
-            air12L.error = 'Please enter number';
-        }
+        const { air12L, air15L, nitrox12L, nitrox15L } = this.props.seed;
         return <div className="checklist-form">
             <h2>Tanks</h2>
             <form >
                 <label> air 12L
                     <TextField seed={air12L} when={this.whenAir12L} />
                 </label>
-                {
-                    air12L.error !== ''
-                        ? <div className="input-error">{air12L.error}</div>
-                        : null
-                }
                 <label> air 15L
-                    <input type="text" placeholder="0" />
+                    <TextField seed={air15L} when={this.whenAir15L} />
                 </label>
                 <label> nitrox 12L
-                    <input type="text" placeholder="0" />
+                    <TextField seed={nitrox12L} when={this.whenNitrox12L} />
                 </label>
                 <label> nitrox 15L
-                    <input type="text" placeholder="0" />
+                    <TextField seed={nitrox15L} when={this.whenNitrox15L} />
                 </label>
                 <button onSubmit={() => {
                     this.props.when({
