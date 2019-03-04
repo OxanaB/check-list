aac9837452665afaf42a95801ca3daf57fb2a427
Oxana Bykova <bykova.oksana@gmail.com>
2019-03-04 14:53:43 -0500

    wip boat select contains two options

--- boat-select.tsx
+++ boat-select.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
-import { DiversBoatChecklist, DiversBoatChecklistConcern, DiversBoatChecklistSeed } from './divers-boat-checklist';
-import { IntroBoatChecklist, IntroBoatChecklistConcern, IntroBoatChecklistSeed } from './intro-boat-checklist';
+import { DiversBoatChecklist, DiversBoatChecklistConcern, DiversBoatChecklistProps, DiversBoatChecklistSeed } from './divers-boat-checklist';
+import { IntroBoatChecklist, IntroBoatChecklistConcern, IntroBoatChecklistProps, IntroBoatChecklistSeed } from './intro-boat-checklist';
 import { SelectField, SelectFieldConcern } from './select-field';
 import { broke } from './utils';
 
 export type BoatConcern = BoatChoiseConcern | DiversBoatChecklistConcern | IntroBoatChecklistConcern;
+
+export interface BoatChoiseConcern {
+    about: 'choose-boat';
+    boat: SelectFieldConcern;
+}
 
 export interface BoatSelectSeed {
     activeOption: string;
     diversBoat: DiversBoatChecklistSeed;
     introBoat: IntroBoatChecklistSeed;
 }
-export interface BoatChoiseConcern {
-    about: 'choose-boat';
-    boat: SelectFieldConcern;
-}
+
 export interface BoatSelectProps {
     seed: BoatSelectSeed;
     when: (concern: BoatConcern) => void;
@@ -26,27 +28,42 @@ export class BoatSelect extends React.Component<BoatSelectProps> {
         this.props.when({ about: 'choose-boat', boat: concern });
     }
     render() {
-        const { seed: { activeOption } } = this.props;
-        const { seed: { tanks, weights, comment } } = this.props;
-        const diverBoatProps:
+        const { seed: { activeOption, diversBoat, introBoat } } = this.props;
+        const diversBoatProps: DiversBoatChecklistProps = {
+            seed: diversBoat,
+            when: concern => {
+                this.props.when(concern);
+            },
+        };
+        const introBoatProps: IntroBoatChecklistProps = {
+            seed: introBoat,
+            when: concern => {
+                this.props.when(concern);
+            },
+        };
         return <>
             <SelectField options={boatOptions} when={this.whenBoatChoosen}
                 activeOption={activeOption} />
             {
                 activeOption === 'divers-boat'
-                ? <DiversBoatChecklist />
-                : <IntroBoatChecklist />
+                    ? <DiversBoatChecklist {...diversBoatProps} />
+                    : <IntroBoatChecklist {...introBoatProps} />
             }
         </>;
     }
 }
 
 export function faceBoatSelectConcern(
-    _boat: BoatSelectSeed,
+    oldBoatProps: BoatSelectSeed,
     concern: BoatConcern,
 ): BoatSelectSeed {
     switch (concern.about) {
-        case 'choose-boat': return concern.boat;
+        case 'choose-boat': {
+            const { diversBoat, introBoat } = oldBoatProps;
+            return {
+                ...diversBoat, ...introBoat, activeOption: concern.boat,
+            };
+        }
         default: return broke(concern.about);
     }
 }
