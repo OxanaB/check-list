bde25ff94154d2561f6e47858fded08c21ac19c6
OxanaB <44169452+OxanaB@users.noreply.github.com>
2019-03-10 09:25:17 -0400

    Delete firebase-config.ts

--- firebase-config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyBIWYE2Uttv-VuDUACzTtrFNWeqiy8C7QE',
-    authDomain: 'checklist-654bf.firebaseapp.com',
-    databaseURL: 'https://checklist-654bf.firebaseio.com',
-    projectId: 'checklist-654bf',
-    storageBucket: 'checklist-654bf.appspot.com',
-    messagingSenderId: '558481055037',
-};
